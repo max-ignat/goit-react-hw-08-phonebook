abec00070c64a569760668c2cb0d3b423d5f92b2
max-ignat <107836285+max-ignat@users.noreply.github.com>
2023-03-10 11:42:01 +0000

    Deploying to gh-pages from @ max-ignat/goit-react-hw-08-phonebook@623d219790d867c8ecc1d9b1df0f6aedec10d322 🚀

---
+++ static/js/747.e6211752.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[747],{4747:function(e,o,n){n.r(o);n(2791);var t=n(184);o.default=function(){return(0,t.jsx)("p",{children:"Home Page"})}}}]);
+//# sourceMappingURL=747.e6211752.chunk.js.map
